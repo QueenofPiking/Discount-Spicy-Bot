48a6d8b23436ab87dfe8d1936018fd018022d062
QueenofPiking <tektronix2002@gmail.com>
2019-10-21 16:06:36 -0500

    e

---
+++ commands/sfw/pat.js
@@ -0,0 +1,24 @@
+const client = require("nekos.life");
+const neko = new client();
+
+
+module.exports = {
+    config: {
+        name: "pat",
+        aliases: [],
+        description: "Pat someone uwu~",
+        usage: "(@user)",
+        category: "general"
+    },
+    run: async(client, message, args) => {
+        let hugurl = await neko.sfw.kiss();
+        let huser = message.guild.member(message.mentions.users.first() ||  message.guild.members.get(args[0]));
+
+        if(huser){
+            message.channel.send(`hey ${huser}, ${message.author} has patted you!!! \n${hugurl.url}`)
+        } else {
+            message.channel.send(`${message.author}, Wha?! Why are you patting ME?! >///////<`)
+        }
+
+    }
+}
