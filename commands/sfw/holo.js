a5b87e062b15c349b57e5d27799d173dd756253c
QueenofPiking <tektronix2002@gmail.com>
2019-10-21 12:33:10 -0500

    eee

---
+++ commands/sfw/holo.js
@@ -0,0 +1,24 @@
+const client = require("nekos.life");
+const neko = new client();
+
+
+module.exports = {
+    config: {
+        name: "holo",
+        aliases: [],
+        description: "placeholder",
+        usage: "(@user)",
+        category: "general"
+    },
+    run: async(client, message, args) => {
+        let hugurl = await neko.sfw.hug();
+        let huser = message.guild.member(message.mentions.users.first() ||  message.guild.members.get(args[0]));
+
+        if(huser){
+            message.channel.send(`hey ${huser}, ${message.author} what the fuck\n${hugurl.url}`)
+        } else {
+            message.channel.send(`${message.author}, cuckold`)
+        }
+
+    }
+}
