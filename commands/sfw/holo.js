77fb68d8f858272b311dc65e98647b1031eddf5f
QueenofPiking <tektronix2002@gmail.com>
2019-10-21 12:34:52 -0500

    eeee

--- commands/sfw/holo.js
+++ commands/sfw/holo.js
@@ -11,7 +11,7 @@ module.exports = {
         category: "general"
     },
     run: async(client, message, args) => {
-        let hugurl = await neko.sfw.hug();
+        let hugurl = await neko.sfw.holo();
         let huser = message.guild.member(message.mentions.users.first() ||  message.guild.members.get(args[0]));
 
         if(huser){
