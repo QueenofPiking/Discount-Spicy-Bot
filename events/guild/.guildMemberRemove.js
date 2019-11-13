7a899e52074e3f976fde4a8284e83da4c001c6ec
QueenofPiking <tektronix2002@gmail.com>
2019-11-13 14:19:23 -0600

    jkbvdkj gfjnbgrfergjnbhrkj

--- events/guild/.guildMemberRemove.js
+++ events/guild/.guildMemberRemove.js
@@ -1,19 +1,23 @@
 module.exports = async (client, member) => {
-    const welcomechannel = member.guild.channels.find(x => x.name === "gatekeeper");
+    //defines the #welcome channel
+    const welcomeChannel = member.guild.channels.find(x => x.name === "gatekeeper");
+    //stores a bunch of values into one variable for later use
     const messages = [
         `**${member.user.username}** left!`,
-        `**${member.user.username}**, Has been smited by Jack`,
+        `**${member.user.username}**, Libtard Destroyed.`,
         `**${member.user.username}** is gone!`,
-        `**${member.user.username}** Commited Deactivation`,
-        `**${member.user.username}** has been hit with the Mind Charge Black Viper`,
-        `**${member.user.username}**, has bluescreened`,
-        `**${member.user.username}** has commited Takaya's Evoker`,
-        `**${member.user.username}** Hit Nyx during Moonless Gown!`,
-        `**${member.user.username}** Tried to solo the Reaper`
+        `**${member.user.username}** didn't suck off Daddy Jack!`,
+        `**${member.user.username}** has been Suspended`,
+        `**${member.user.username}**, Has Alt F4ed IRL!`,
+        `**${member.user.username}** Was raped by Mara!`,
+        `**${member.user.username}** went to go solo the Reaper!`,
+        `**${member.user.username}** challenged Strega`,
+        `**${member.user.username}** found out what happens when you become Akihiko and inject straight protein into your blood stream`
     ];
-    if(!welcomechannel) return;
-    const message = Math.floor(Math.random() * (message.length - 1) + 1);
-    welcomechannel.send(messages[message]);
-
-
+    //checks if the #welcome channel exists
+    if(!welcomeChannel) return;
+    //defines a message as a random value and selects the value from the array above
+    const message = Math.floor(Math.random() * (messages.length - 1) + 1);
+    //sends the random value to the #welcome channel
+    welcomeChannel.send(messages[message]);
 }
