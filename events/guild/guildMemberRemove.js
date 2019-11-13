2861b1ea812d182697119123cbea63649d9a7e9f
QueenofPiking <tektronix2002@gmail.com>
2019-11-13 14:24:26 -0600

    e

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,7 +1,7 @@
 module.exports = async (client, member) => {
-    //defines the #welcome channel
+    
     const welcomeChannel = member.guild.channels.find(x => x.name === "gatekeeper");
-    //stores a bunch of values into one variable for later use
+    
     const messages = [
         `**${member.user.username}** left!`,
         `**${member.user.username}**, Libtard Destroyed.`,
@@ -14,10 +14,10 @@ module.exports = async (client, member) => {
         `**${member.user.username}** challenged Strega`,
         `**${member.user.username}** found out what happens when you become Akihiko and inject straight protein into your blood stream`
     ];
-    //checks if the #welcome channel exists
+    
     if(!welcomeChannel) return;
-    //defines a message as a random value and selects the value from the array above
+    
     const message = Math.floor(Math.random() * (messages.length - 1) + 1);
-    //sends the random value to the #welcome channel
+    
     welcomeChannel.send(messages[message]);
 }
