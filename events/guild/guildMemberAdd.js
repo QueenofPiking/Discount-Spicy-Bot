1c71d699bec3ded815e8904d53d21ee72bf1a51c
QueenofPiking <tektronix2002@gmail.com>
2019-12-01 20:54:06 -0600

    ee

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,9 +1,9 @@
 module.exports = async (client, member) => {
     const welcomechannel = member.guild.channels.find(x => x.name === "gatekeeper")
-    const role = member.guild.roles.find(x => x.name === "Peasant")
+    const role = member.guild.roles.find(x => x.name === "Phantom Thieves")
     if(!role) return;
     if(!welcomechannel) return;
-    welcomechannel.send(`Welcome to this hellhole ${member} Make sure to read the rules!`)
+    welcomechannel.send(`Welcome to the Metaverse! ${member} Make sure to read the rules or you will be crushed by Mara! `)
     member.addRole(role.id);
     return;
 }
