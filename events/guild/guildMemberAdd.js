2c4097626c608aaec1fcf0d9616de27083683eab
QueenofPiking <tektronix2002@gmail.com>
2019-12-17 16:49:13 -0600

    dw

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,6 +1,7 @@
 module.exports = async (client, member) => {
     const welcomechannel = member.guild.channels.find(x => x.name === "gatekeeper")
     const role = member.guild.roles.find(x => x.name === "Shujin Academy Student")
+    const role = member.guild.roles.find(x => x.name === "Follower")
     if(!role) return;
     if(!welcomechannel) return;
     welcomechannel.send(`Welcome to the Server! ${member} Make sure to read the rules or you will be hit by a Megidolaon! `)
