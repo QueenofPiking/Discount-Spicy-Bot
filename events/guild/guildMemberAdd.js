f6a9242eff7b11f73a74aed7525da5579c2edcc2
QueenofPiking <tektronix2002@gmail.com>
2019-12-18 09:52:34 -0600

    fwe

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -2,10 +2,10 @@ module.exports = async (client, member) => {
     var welcomechannel = member.guild.channels.find(x => x.name === "gatekeeper")
     var role = member.guild.roles.find(x => x.name === "Shujin Academy Student")
     let role1 = member.guild.roles.find(x => x.name === "Shujin Academy Student");
-    if (member.guild.id === "650885407352815616") member.addRole(Shujin Academy Student);
+    if (member.guild.id === "650885407352815616") member.addRole(role1);
     var role = member.guild.roles.find(x => x.name === "Follower")
     let role2 = member.guild.roles.find(x => x.name === "Follower");
-    if (member.guild.id === "656301713803837452") member.addRole (Follower);
+    if (member.guild.id === "656301713803837452") member.addRole (role2);
     if(!role) return;
     if(!welcomechannel) return;
     welcomechannel.send(`Welcome to the Server! ${member} Make sure to read the rules or you will be hit by a Megidolaon! `)
