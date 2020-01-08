d8f1c2d39e13b202499fbbb23b5e4e0b9a684519
QueenofPiking <tektronix2002@gmail.com>
2020-01-08 12:21:40 -0600

    2

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -12,7 +12,7 @@ module.exports = async (client, member) => {
         `**${member.user.username}** Said P5 Was overrated`,
         `**${member.user.username}** went to go solo the Reaper!`,
         `**${member.user.username}** challenged Strega`,
-        `**${member.user.username}** found out what happens when you become Akihiko and inject straight protein into your blood stream`,
+        `**${member.user.username}** found out what happens when you piss off Makoto Stans`,
         `**${member.user.username}** Joined the SMT Fanbase`,
         `**${member.user.username}** Got the Persona 1 and Persona 2 treatment`
     ];
