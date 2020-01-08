d7137148b8d4dfeaf221c88a94e5a69eebc6cbd8
QueenofPiking <tektronix2002@gmail.com>
2020-01-08 12:23:14 -0600

    qweqweqweqweqweqweqwe

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -9,12 +9,12 @@ module.exports = async (client, member) => {
         `**${member.user.username}** Fought the Indolent Maya!`,
         `**${member.user.username}** has been executed by Mitsuru!`,
         `**${member.user.username}** Was hit by a Mind Charge Black Viper!`,
-        `**${member.user.username}** Said P5 Was overrated`,
+        `**${member.user.username}** Said P5 Was overrated!`,
         `**${member.user.username}** went to go solo the Reaper!`,
-        `**${member.user.username}** challenged Strega`,
-        `**${member.user.username}** found out what happens when you piss off Makoto Stans`,
-        `**${member.user.username}** Joined the SMT Fanbase`,
-        `**${member.user.username}** Got the Persona 1 and Persona 2 treatment`
+        `**${member.user.username}** got hunted by Strega!`,
+        `**${member.user.username}** found out what happens when you piss off Makoto Stans!`,
+        `**${member.user.username}** Joined the SMT Fanbase!`,
+        `**${member.user.username}** Got the Persona 1 and Persona 2 treatment!`
     ];
     
     if(!welcomeChannel) return;
