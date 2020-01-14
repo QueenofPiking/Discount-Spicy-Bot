7f5c1d93bdfcd494766b995756390d691a618d5b
QueenofPiking <tektronix2002@gmail.com>
2020-01-14 14:32:50 -0600

    cumblaster79

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -4,16 +4,16 @@ module.exports = async (client, member) => {
     
     const messages = [
         `**${member.user.username}** Became Door-kun!~`,
-        `**${member.user.username}** just got shot by Takaya!`,
+        `**${member.user.username}** Attempted to best Nyarlathotep!`,
         `**${member.user.username}** Went and pulled a Shinjiro!`,
         `**${member.user.username}** Fought the Indolent Maya!`,
         `**${member.user.username}** has been executed by Mitsuru!`,
-        `**${member.user.username}** Was hit by a Mind Charge Black Viper!`,
-        `**${member.user.username}** Said P5 Was overrated!`,
+        `**${member.user.username}** Was hit by a Hassou Tobi!`,
+        `**${member.user.username}** Posted Cringe!`,
         `**${member.user.username}** went to go solo the Reaper!`,
-        `**${member.user.username}** got hunted by Strega!`,
+        `**${member.user.username}** got hunted down by Strega!`,
         `**${member.user.username}** found out what happens when you piss off Makoto Stans!`,
-        `**${member.user.username}** Joined the SMT Fanbase!`,
+        `**${member.user.username}** Tried to fight Demi-Fiend!`,
         `**${member.user.username}** Got the Persona 1 and Persona 2 treatment!`
         `**${member.user.username}** Got stored into the ATLUS Vault!`
     ];
--- events/guild/message.js
+++ events/guild/message.js
@@ -2,7 +2,7 @@ const { prefix } = require("../../config.json");
 
 module.exports = async (client, message) => {
     if(message.author.bot) return;
-    if(message.channel.type === 'dm') return message.reply("You must be in a server to use me! I cannot do any commands via direct message!");
+    if(message.channel.type === 'dm') return message.reply("You must be in a server to use me! I cannot execute any commands via direct message!");
     
 
     let args = message.content.slice(prefix.length).trim().split(/ +/g)
