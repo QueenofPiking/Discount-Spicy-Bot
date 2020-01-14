96f968612cc6944cb2b049ddf8a77a3de16c14b0
QueenofPiking <tektronix2002@gmail.com>
2020-01-14 09:36:38 -0600

    ty

---
+++ commands/mod/mute.js
@@ -0,0 +1,12 @@
+module.exports = {
+    config: {
+        name: "mute",
+        aliases: ["m"],
+        description: "Mute anyone being troublesome",
+        usage: "@user",
+        category: "mod"
+    },
+    run: async (client, message, args) => {
+
+    }
+}
