90899dc8611f40830300c63148e84617e17b4a2a
QueenofPiking <tektronix2002@gmail.com>
2020-01-18 18:04:53 -0600

    cum in my daddy

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -4,6 +4,8 @@ module.exports = async (client, member) => {
     if (member.guild.id === "650885407352815616") member.addRole(role1);
     let role2 = member.guild.roles.find(x => x.name === "Follower");
     if (member.guild.id === "656301713803837452") member.addRole(role2);
+    let role3 = member.guild.roles.find(x => x.name === "Hot Thottie");
+    if (member.guild.id === "668220215196123187") member.addRole(role3);
     if(!welcomechannel) return;
     welcomechannel.send(`Welcome to the Server! ${member} Make sure to read the rules or you will be hit by a Megidolaon! And have an amazing time in the server! Please @ or DM a staff member if you need assistance!`)
     return;
