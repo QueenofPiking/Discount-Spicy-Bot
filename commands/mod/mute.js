d16874549b0c832252cded5e156cba1bb323f58d
QueenofPiking <tektronix2002@gmail.com>
2020-01-21 11:47:39 -0600

    cumblaster79

--- commands/mod/mute.js
+++ commands/mod/mute.js
@@ -12,10 +12,26 @@ module.exports = {
     run: async (client, message, args) => {
         await message.delete();
         let muted = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!muted) return message.reply("Give a specific @ please!")
-        if(muted.id === message.author.id) return message.reply(`Actually, Quantum Mechanics forbids this.`, {
+        if (!muted) return message.reply("Give a specific @ please!")
+        if (muted.id === message.author.id) return message.reply(`Actually, Quantum Mechanics forbids this.`, {
             file: "https://i.kym-cdn.com/entries/icons/original/000/026/270/actually.jpg"
         });
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Shut the fuck up")
 
+        try {
+            message.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermissions(muted, {
+                    VIEW_CHANNEL: null,
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false,
+                    CONNECT: true,
+                    SPEAK: false,
+                });
+            });
+        } catch (e) {
+            if(e) console.log(e.stack);
+        }
+
+        return message.reply(`${muted} has been silenced!`)
     }
 }
