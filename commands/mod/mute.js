004a582fd2065754be0109a02e274870324542c1
QueenofPiking <tektronix2002@gmail.com>
2020-01-21 11:30:55 -0600

    sad

--- commands/mod/mute.js
+++ commands/mod/mute.js
@@ -1,3 +1,6 @@
+const { Attachment } = require("discord.js");
+let picture = new Attachment("https://i.kym-cdn.com/entries/icons/original/000/026/270/actually.jpg", "picture.jpg");
+
 module.exports = {
     config: {
         name: "mute",
@@ -7,6 +10,10 @@ module.exports = {
         category: "mod"
     },
     run: async (client, message, args) => {
-
+        await message.delete();
+        let muted = message.guild.member(message.mentions.user.first() || message.guild.members.get(args[0]));
+        if(!muted) return message.reply("Give a specific @ please!")
+        if(muted.id === message.author.id) return message.reply(`Actually, Quantum Mechanics forbids this. ${picture}`);
+        
     }
 }
