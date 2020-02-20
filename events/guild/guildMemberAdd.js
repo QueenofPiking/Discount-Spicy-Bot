bc8341624ee9a3e8d0329b1e10f86652060b01c4
QueenofPiking <Tektronix2002@gmail.com>
2020-02-20 12:57:34 -0800

    Metaverse Traveler added over old

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,6 +1,6 @@
 module.exports = async (client, member) => {
     var welcomechannel = member.guild.channels.find(x => x.name === "gatekeeper")
-    let role1 = member.guild.roles.find(x => x.name === "Shujin Academy Student");
+    let role1 = member.guild.roles.find(x => x.name === "Metaverse Traveler");
     if (member.guild.id === "650885407352815616") member.addRole(role1);
     let role2 = member.guild.roles.find(x => x.name === "Follower");
     if (member.guild.id === "656301713803837452") member.addRole(role2);
